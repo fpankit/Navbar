5d3af53153e89e4acedcf81f08b59e8bfd786337
Students <students@SAITMs-iMac-2.local>
2024-04-30 10:55:13 +0530

    push

---
+++ Navbar/Simple NavigationBar/Simple NavigationBar/OneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OneViewController.swift
+//  Simple NavigationBar
+//
+//  Created by Students on 4/30/24.
+//  Copyright © 2024 RDC. All rights reserved.
+//
+
+import UIKit
+
+class OneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
